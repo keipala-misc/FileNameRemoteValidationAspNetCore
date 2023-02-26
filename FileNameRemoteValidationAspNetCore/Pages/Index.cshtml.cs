20c249e49b9da15051e9368c943f36f53fc7edb6
keipala <keipalahniuk@gmail.com>
2023-02-26 11:10:05 +0200

    Add file input with custom type validation.

--- FileNameRemoteValidationAspNetCore/Pages/Index.cshtml.cs
+++ FileNameRemoteValidationAspNetCore/Pages/Index.cshtml.cs
@@ -2,31 +2,48 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 
-namespace FileNameRemoteValidationAspNetCore.Pages
+namespace FileNameRemoteValidationAspNetCore.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    [Required]
+    [PdfFileType(ErrorMessage = "Please select a PDF file.")]
+    public List<IFormFile> Files { get; set; }
+
+    public IndexModel()
     {
-        private readonly ILogger<IndexModel> _logger;
-        [BindProperty]
-        public string Name { get; set; }
+    }
 
-        public IndexModel(ILogger<IndexModel> logger)
+    public void OnGet()
+    {
+
+    }
+
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
         {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-        }
-
-        public IActionResult OnPost()
-        {
-            if (ModelState.IsValid)
-            {
-                return Page();
-            }
             return Page();
         }
+        return Page();
+    }
+}
+
+public class PdfFileTypeAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var files = value as List<IFormFile>;
+        if (files != null)
+        {
+            foreach (var file in files)
+            {
+                if (file.ContentType != "application/pdf")
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+        }
+        return ValidationResult.Success;
     }
 }

fcb832acd62dbba51b185188271a8333b1e56d1d
keipala <keipalahniuk@gmail.com>
2023-02-26 16:46:40 +0200

    Working example

--- FileNameRemoteValidationAspNetCore/Pages/Index.cshtml.cs
+++ FileNameRemoteValidationAspNetCore/Pages/Index.cshtml.cs
@@ -11,8 +11,14 @@ public class IndexModel : PageModel
     [PdfFileType(ErrorMessage = "Please select a PDF file.")]
     public List<IFormFile> Files { get; set; }
 
+    [PageRemote(
+        ErrorMessage = "Name must end with .pdf",
+        HttpMethod = "POST",
+        AdditionalFields = "__RequestVerificationToken",
+        PageHandler = "CheckFileNames"
+    )]
     [BindProperty]
-    [FileName(ErrorMessage = "Name must end with .pdf")]
+    //[FileName(ErrorMessage = "Name must end with .pdf")]
     public string FileNames { get; set; }
 
     public IndexModel()
@@ -21,7 +27,15 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+    }
 
+    public JsonResult OnPostCheckFileNames()
+    {
+        if (FileNames.ToLower().EndsWith(".pdf"))
+        {
+            return new JsonResult("String ends with .pdf");
+        }
+        return new JsonResult(true);
     }
 
     public IActionResult OnPost()
@@ -54,21 +68,22 @@ public class PdfFileTypeAttribute : ValidationAttribute
     }
 }
 
-public class FileNameAttribute : ValidationAttribute
-{
-    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-    {
-        if (value is string fileNamesString)
-        {
-            var fileNames = fileNamesString.Split("/");
-            foreach (var name in fileNames)
-            {
-                if (name.ToLower().EndsWith(".pdf"))
-                {
-                    return new ValidationResult(ErrorMessage);
-                }
-            }
-        }
-        return ValidationResult.Success;
-    }
-}
+// removed because I don't think I can use it for my purposes
+//public class FileNameAttribute : ValidationAttribute
+//{
+//    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+//    {
+//        if (value is string fileNamesString)
+//        {
+//            var fileNames = fileNamesString.Split("/");
+//            foreach (var name in fileNames)
+//            {
+//                if (name.ToLower().EndsWith(".pdf"))
+//                {
+//                    return new ValidationResult(ErrorMessage);
+//                }
+//            }
+//        }
+//        return ValidationResult.Success;
+//    }
+//}
